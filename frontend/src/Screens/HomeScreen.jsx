7cff4c91b93fffd67a806e2d3fc844dca5f51901
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2024-01-08 23:47:35 +0600

    add rating

--- frontend/src/Screens/HomeScreen.jsx
+++ frontend/src/Screens/HomeScreen.jsx
@@ -5,8 +5,8 @@ import products from '../products'
 const HomeScreen = () => {
   return (
     <Container sx={{ my: 5 }}>
-      <Typography variant='h4' gutterBottom>
-        My Product Grid
+      <Typography variant='h5' gutterBottom>
+        Latest Product List
       </Typography>
       <Grid container spacing={2}>
         {products.map((product, index) => (
---
+++ frontend/src/components/Rating.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {StarIcon} from '@mui/icons-material/';
+
+const Rating = ({value , text}) => {
+    return (
+        <div>
+            {value => 1 ?  }
+        </div>
+    );
+};
+
+export default Rating;
