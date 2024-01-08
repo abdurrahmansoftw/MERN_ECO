ad7116032ec7f1d481fecc4fb0c8a31a0b8f170f
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2024-01-08 23:01:57 +0600

    eslint

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { AppBar, Box, Button, Toolbar, Typography } from '@mui/material'
 const Navbar = () => {
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position='static'>
+      <AppBar position='static' elevation={1}>
         <Toolbar>
           <Typography variant='h6' component='div' sx={{ flexGrow: 1 }}>
             LOGO
