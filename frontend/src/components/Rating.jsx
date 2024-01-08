83f9cc90fa0fd628051cc7d3478fee6421ed8114
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2024-01-08 23:53:30 +0600

    Rating

--- frontend/src/components/Rating.jsx
+++ frontend/src/components/Rating.jsx
@@ -1,12 +1,62 @@
-import React from 'react';
-import {StarIcon} from '@mui/icons-material/';
+import { StarBorderIcon, StarHalfIcon, StarIcon } from '@mui/icons-material/'
+import { Box } from '@mui/material'
 
-const Rating = ({value , text}) => {
-    return (
-        <div>
-            {value => 1 ?  }
-        </div>
-    );
-};
+const Rating = ({ value, text }) => {
+  return (
+    <Box>
+      <span>
+        {value >= 1 ? (
+          <StarIcon />
+        ) : value >= 0.5 ? (
+          <StarHalfIcon />
+        ) : (
+          <StarBorderIcon />
+        )}
+      </span>
 
-export default Rating;
+      <span>
+        {value >= 2 ? (
+          <StarIcon />
+        ) : value >= 1.5 ? (
+          <StarHalfIcon />
+        ) : (
+          <StarBorderIcon />
+        )}
+      </span>
+
+      <span>
+        {value >= 3 ? (
+          <StarIcon />
+        ) : value >= 2.5 ? (
+          <StarHalfIcon />
+        ) : (
+          <StarBorderIcon />
+        )}
+      </span>
+
+      <span>
+        {value >= 4 ? (
+          <StarIcon />
+        ) : value >= 3.5 ? (
+          <StarHalfIcon />
+        ) : (
+          <StarBorderIcon />
+        )}
+      </span>
+
+      <span>
+        {value >= 5 ? (
+          <StarIcon />
+        ) : value >= 4.5 ? (
+          <StarHalfIcon />
+        ) : (
+          <StarBorderIcon />
+        )}
+      </span>
+
+      <span className='text-nowrap'> {text && text} </span>
+    </Box>
+  )
+}
+
+export default Rating
