f8fa82f88a4c4320ba57367742c805a1719aa652
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2024-01-09 16:00:20 +0600

    Error

--- frontend/src/Screens/HomeScreen.jsx
+++ frontend/src/Screens/HomeScreen.jsx
@@ -8,11 +8,11 @@ const HomeScreen = () => {
 
   useEffect(() => {
     const fetchProducts = async () => {
-      const { data } = await axios.get('http://localhost:5000/api/products')
+      const { data } = await axios.get('/api/products')
       setProducts(data)
     }
     fetchProducts()
-  }, [])
+  }, [products])
 
   return (
     <Box sx={{ my: 5 }}>
