3a59273a46a5eba18b03160fd8a98194f42c48a4
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2024-01-09 18:51:33 +0600

    productController

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,3 +1,25 @@
 import asyncHandler from '../middleware/asyncHandler.js'
+import Product from '../models/productModel.js'
 
-const getProducts = asyncHandler(async (req, res) => {})
+// @docs    Fetch all products
+// @route   GET /api/products
+// @access  Public
+const getProducts = asyncHandler(async (req, res) => {
+  const products = await Product.find({})
+  res.json(products)
+})
+
+// @docs    Fetch single product
+// @route   GET /api/products/:id
+// @access  Public
+const getProductById = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id)
+
+  if (product) return res.json(product)
+  else {
+    res.status(404)
+    throw new Error('Product not found')
+  }
+})
+
+export { getProductById, getProducts }
