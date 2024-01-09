2c46b18f7dd3202f51d9b9c3970192a48922ee68
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2024-01-09 23:13:26 +0600

    constants

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
 import { RouterProvider } from 'react-router-dom'
 import './index.css'
 import router from './routes/router'
+import store from './store'
 import ThemeProvider from './theme'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider>
-      <RouterProvider router={router} />
+      <Provider store={store}>
+        <RouterProvider router={router} />
+      </Provider>
     </ThemeProvider>
   </React.StrictMode>
 )
