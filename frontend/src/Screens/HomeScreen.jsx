164083bd9a2b6dd38fc0fc02046375c6d2d9d828
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2024-01-09 23:42:05 +0600

    baseQuery

--- frontend/src/Screens/HomeScreen.jsx
+++ frontend/src/Screens/HomeScreen.jsx
@@ -1,24 +1,21 @@
 import { Box, Grid, Typography } from '@mui/material'
-import axios from 'axios'
-import { useEffect, useState } from 'react'
 import ProductCard from '../components/ProductCard'
+import { useGetProductsQuery } from '../slices/productsApiSlice'
 
 const HomeScreen = () => {
-  const [products, setProducts] = useState([])
-
-  useEffect(() => {
-    const fetchProducts = async () => {
-      const { data } = await axios.get('/api/products')
-      setProducts(data)
-    }
-    fetchProducts()
-  }, [])
+  const { data: products, isLoading, isError } = useGetProductsQuery()
 
   return (
     <Box sx={{ my: 5 }}>
       <Typography variant='h5' gutterBottom>
         Latest Product List
       </Typography>
+
+      {isLoading && <Typography variant='h5'>Loading...</Typography>}
+      {isError && (
+        <Typography variant='h5'> {isError.data.message} </Typography>
+      )}
+
       <Grid container spacing={2}>
         {products.map((product, index) => (
           <Grid item key={index} xs={12} sm={6} md={4} lg={4} xl={3}>
--- frontend/src/slices/apiSlice.js
+++ frontend/src/slices/apiSlice.js
@@ -1,10 +1,11 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query'
 import { BASE_URL } from '../constants'
 
-const baseUrl = fetchBaseQuery({ baseUrl: BASE_URL })
-
+const baseQuery = fetchBaseQuery({
+  baseUrl: BASE_URL,
+})
 export const apiSlice = createApi({
-  baseUrl,
+  baseQuery,
   tagTypes: ['Product', 'User', 'Order'],
   endpoints: (builder) => ({}),
 })
