87025881e8a902f0a75dd0386306d816978b7e55
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2024-01-10 23:30:38 +0600

    register

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,26 +1,19 @@
-import jwt from 'jsonwebtoken'
 import asyncHandler from '../middleware/asyncHandler.js'
 import User from '../models/userModel.js'
+import generateToken from '../utils/generateToken.js'
 
 // @desc    Auth user & get token
 // @route   POST /api/users/auth
 // @access  Public
 const authUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body
+
   const user = await User.findOne({ email })
-  console.log('user', user)
 
   if (user && (await user.matchPassword(password))) {
-    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
-      expiresIn: '30d',
-    })
+    generateToken(res, user._id)
 
-    res.json({
-      _id: user._id,
-      name: user.name,
-      email: user.email,
-      isAdmin: user.isAdmin,
-    })
+    res.json({ _id: user._id, name: user.name, email: user.email, isAdmin: user.isAdmin })
   } else {
     res.status(401)
     throw new Error('Invalid email or password')
