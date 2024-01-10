b1f6beffc30f3d91565c10c3bc08f0fbcca2f3f0
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2024-01-10 11:38:33 +0600

    calculate total price

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -31,10 +31,16 @@ const cartSlice = createSlice({
       )
       // calculate shipping price
       state.shippingPrice = addDecimals(state.itemsPrice > 100 ? 0 : 10)
-      
+
       // calculate tax price
       state.taxPrice = addDecimals(Number((0.15 * state.itemsPrice).toFixed(2)))
-      
+
+      // calculate total price
+      state.totalPrice = (
+        Number(state.itemsPrice) +
+        Number(state.shippingPrice) +
+        Number(state.taxPrice)
+      ).toFixed(2)
     },
   },
 })
