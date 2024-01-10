fb81966c16afc6c7ef3ed881c984881e4bef5126
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2024-01-10 11:29:18 +0600

    addTocart

---
+++ frontend/src/slices/cartSlice.js
@@ -0,0 +1,19 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = localStorage.getItem('cart')
+  ? JSON.parse(localStorage.getItem('cart'))
+  : { cartItems: [] }
+
+const cartSlice = createSlice({
+  name: 'cart',
+  initialState,
+  reducers: {
+    addTocart: (state, action) => {
+      const item = action.payload
+
+      const existItem = state.cartItems.find((x) => x._id === item._id)
+    },
+  },
+})
+
+export default cartSlice.reducer
