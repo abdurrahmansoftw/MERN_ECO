27579c4854de179a182d277e6c07130fb8f474fb
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2024-01-10 11:37:58 +0600

    items price

--- frontend/src/Skeleton/ProductSkeleton.jsx
+++ frontend/src/Skeleton/ProductSkeleton.jsx
@@ -5,7 +5,7 @@ const ProductSkeleton = () => {
   return (
     <Grid container spacing={2}>
       <Grid item xs={12} sm={6} md={4} lg={4} xl={3}>
-        <Skeleton sx={{ bgcolor: 'grey.900' }} variant='rectangular' />
+        <Skeleton sx={{ bgcolor: 'grey.900' }} />
       </Grid>
     </Grid>
   )
