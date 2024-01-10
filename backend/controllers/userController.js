53dbf7df8f99c53d7bd6b8712815b0f2932e1a04
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2024-01-10 23:05:59 +0600

    cookie error

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -41,7 +41,11 @@ const registerUser = asyncHandler(async (req, res) => {
 // @route   POST /api/users/logout
 // @access  Public
 const logoutUser = (req, res) => {
-  res.send('logout User')
+  res.clearCookie('jwt', '', {
+    httpOnly: true,
+    expires: new Date(0),
+  })
+  res.status(200).json({ message: 'Logged out Successfully ' })
 }
 
 // @desc    Get user profile
