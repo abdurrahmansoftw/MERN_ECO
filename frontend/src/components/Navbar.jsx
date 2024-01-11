5c0481c6c046105d77b931e326d2c6dee28faf00
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2024-01-11 13:33:08 +0600

    dropdnown

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,11 +1,24 @@
+import AccountCircleIcon from '@mui/icons-material/AccountCircle'
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart'
-import { AppBar, Badge, Box, Button, Toolbar, Typography } from '@mui/material'
+import { AppBar, Badge, Box, Button, Divider, Menu, MenuItem, Toolbar, Typography } from '@mui/material'
+import Fade from '@mui/material/Fade'
+import { useState } from 'react'
 import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   const { cartItems } = useSelector((state) => state.cart)
-  console.log(cartItems)
+  const { userInfo } = useSelector((state) => state.auth)
+
+  const [anchorEl, setAnchorEl] = useState(null)
+  const open = Boolean(anchorEl)
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget)
+  }
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position='static' elevation={1}>
@@ -27,9 +40,36 @@ const Navbar = () => {
               </Box>
             )}
           </Button>
-          <Button color='inherit' component={Link} to='/login'>
-            Login
-          </Button>
+
+          {userInfo ? (
+            <div>
+              <Button
+                color='inherit'
+                id='fade-button'
+                aria-controls={open ? 'fade-menu' : undefined}
+                aria-haspopup='true'
+                aria-expanded={open ? 'true' : undefined}
+                onClick={handleClick}>
+                <AccountCircleIcon fontSize='small' /> {userInfo.name}
+              </Button>
+              <Menu
+                id='fade-menu'
+                MenuListProps={{ 'aria-labelledby': 'fade-button' }}
+                anchorEl={anchorEl}
+                open={open}
+                onClose={handleClose}
+                TransitionComponent={Fade}>
+                <MenuItem onClick={handleClose}>{userInfo.name}</MenuItem>
+                <Divider />
+                <MenuItem onClick={handleClose}>Profile</MenuItem>
+                <MenuItem onClick={handleClose}>Logout</MenuItem>
+              </Menu>
+            </div>
+          ) : (
+            <Button color='inherit' component={Link} to='/login'>
+              Login
+            </Button>
+          )}
         </Toolbar>
       </AppBar>
     </Box>
