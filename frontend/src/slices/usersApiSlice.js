b1d2918e11f3614739cbe4e1fb1799475bed01c3
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2024-01-11 11:37:52 +0600

    FromContainer

--- frontend/src/slices/usersApiSlice.js
+++ frontend/src/slices/usersApiSlice.js
@@ -5,6 +5,7 @@ export const usersApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
     login: builder.mutation({
       query: (data) => ({
+        // eslint-disable-next-line no-undef
         url: USERS_URL / auth,
         method: 'POST',
         body: data,
