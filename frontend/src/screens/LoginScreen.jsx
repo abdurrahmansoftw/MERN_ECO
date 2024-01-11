44a4bbcd570ea3722d9344505718a6addafe677a
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2024-01-11 12:24:05 +0600

    RegisterScreen

--- frontend/src/screens/LoginScreen.jsx
+++ frontend/src/screens/LoginScreen.jsx
@@ -2,6 +2,7 @@ import LockOutlinedIcon from '@mui/icons-material/LockOutlined'
 import React from 'react'
 
 import { Avatar, Box, Button, Checkbox, FormControlLabel, Grid, TextField, Typography } from '@mui/material'
+import FromContainer from '../components/FromContainer'
 import CustomLink from '../components/Link'
 
 const LoginScreen = () => {
@@ -14,52 +15,54 @@ const LoginScreen = () => {
     })
   }
   return (
-    <Box sx={{ marginTop: 8, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-      <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-        <LockOutlinedIcon />
-      </Avatar>
-      <Typography component='h1' variant='h5'>
-        Sign in
-      </Typography>
-      <Box component='form' onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-        <TextField
-          margin='normal'
-          required
-          fullWidth
-          id='email'
-          label='Email Address'
-          name='email'
-          autoComplete='email'
-          autoFocus
-        />
-        <TextField
-          margin='normal'
-          required
-          fullWidth
-          name='password'
-          label='Password'
-          type='password'
-          id='password'
-          autoComplete='current-password'
-        />
-        <FormControlLabel control={<Checkbox value='remember' color='primary' />} label='Remember me' />
-        <Button type='submit' fullWidth variant='contained' sx={{ mt: 3, mb: 2 }}>
-          Sign In
-        </Button>
-        <Grid container>
-          <Grid item xs>
-            <CustomLink to='/forgotpassword' variant='body2'>
-              Forgot password?
-            </CustomLink>
+    <FromContainer>
+      <Box sx={{ marginTop: 8, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+        <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component='h1' variant='h5'>
+          Sign in
+        </Typography>
+        <Box component='form' onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+          <TextField
+            margin='normal'
+            required
+            fullWidth
+            id='email'
+            label='Email Address'
+            name='email'
+            autoComplete='email'
+            autoFocus
+          />
+          <TextField
+            margin='normal'
+            required
+            fullWidth
+            name='password'
+            label='Password'
+            type='password'
+            id='password'
+            autoComplete='current-password'
+          />
+          <FormControlLabel control={<Checkbox value='remember' color='primary' />} label='Remember me' />
+          <Button type='submit' fullWidth variant='contained' sx={{ mt: 3, mb: 2 }}>
+            Sign In
+          </Button>
+          <Grid container>
+            <Grid item xs>
+              <CustomLink to='/forgotpassword' variant='body2'>
+                Forgot password?
+              </CustomLink>
+            </Grid>
+            <Grid item>
+              <CustomLink to='/register' variant='body2'>
+                {"Don't have an account? Sign Up"}
+              </CustomLink>
+            </Grid>
           </Grid>
-          <Grid item>
-            <CustomLink to='/register' variant='body2'>
-              {"Don't have an account? Sign Up"}
-            </CustomLink>
-          </Grid>
-        </Grid>
+        </Box>
       </Box>
-    </Box>
+    </FromContainer>
   )
 }
 
