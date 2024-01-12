a1a7a9b28287c4ce87da3146efaf435c0171e1ea
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2024-01-12 08:03:06 +0600

    navigate

--- frontend/src/screens/ShippingScreen.jsx
+++ frontend/src/screens/ShippingScreen.jsx
@@ -22,6 +22,7 @@ const ShippingScreen = () => {
   const submitHandler = (e) => {
     e.preventDefault()
     dispatch(saveShippingAddress({ address, city, postalCode, country }))
+    navigate('/payment')
   }
 
   return (
