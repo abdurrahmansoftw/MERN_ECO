714cfeb7620eba3454ef8c3bd3b04dddb84eeb28
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2024-01-12 07:27:57 +0600

    shippingAddress

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -1,7 +1,9 @@
 import { createSlice } from '@reduxjs/toolkit'
 import { updateCart } from '../utils/cartUtils'
 
-const initialState = localStorage.getItem('cart') ? JSON.parse(localStorage.getItem('cart')) : { cartItems: [] }
+const initialState = localStorage.getItem('cart')
+  ? JSON.parse(localStorage.getItem('cart'))
+  : { cartItems: [], shippingAddress: {}, }
 
 const cartSlice = createSlice({
   name: 'cart',
@@ -12,7 +14,9 @@ const cartSlice = createSlice({
       const existItem = state.cartItems.find((x) => x._id === item._id)
 
       if (existItem) {
-        state.cartItems = state.cartItems.map((x) => (x._id === existItem._id ? item : x))
+        state.cartItems = state.cartItems.map((x) =>
+          x._id === existItem._id ? item : x
+        )
       } else {
         state.cartItems = [...state.cartItems, item]
       }
@@ -20,6 +24,10 @@ const cartSlice = createSlice({
     },
     removeFromCart: (state, action) => {
       state.cartItems = state.cartItems.filter((x) => x._id !== action.payload)
+      return updateCart(state)
+    },
+    saveShippingAddress: (state, action) => {
+      state.shippingAddress = action.payload
       return updateCart(state)
     },
   },
