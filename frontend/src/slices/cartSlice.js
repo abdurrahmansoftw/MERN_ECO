ad01eeb57dc5b7c587c93cb2a121e7fb3b72fa36
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2024-01-12 07:28:33 +0600

    saveShippingAddress

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -33,6 +33,6 @@ const cartSlice = createSlice({
   },
 })
 
-export const { addToCart, removeFromCart } = cartSlice.actions
+export const { addToCart, removeFromCart , saveShippingAddress} = cartSlice.actions
 
 export default cartSlice.reducer
