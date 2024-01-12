d68aa071ad611dc12b9f382fdc853efb66f86545
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2024-01-12 10:19:18 +0600

    submitHandler

--- frontend/src/screens/PaymentScreen.jsx
+++ frontend/src/screens/PaymentScreen.jsx
@@ -11,6 +11,7 @@ import { useDispatch, useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 import CheckoutStep from '../components/CheckoutStep'
 import FromContainer from '../components/FromContainer'
+import { savePaymentMethod } from '../slices/cartSlice'
 
 const PaymentScreen = () => {
   const [paymentMethod, setPaymentMethod] = useState('PayPal')
@@ -28,6 +29,12 @@ const PaymentScreen = () => {
     }
   }, [shippingAddress, navigate])
 
+  const submitHandler = (e) => {
+    e.preventDefault()
+    dispatch(savePaymentMethod(paymentMethod))
+    navigate('/placeorder')
+  }
+
   return (
     <FromContainer>
       <Box
@@ -43,33 +50,38 @@ const PaymentScreen = () => {
 
         <CheckoutStep stepOne stepTwo stepThree />
 
-        <RadioGroup name='use-radio-group' defaultValue='first'>
-          <Box sx={{ display: 'flex', flexDirection: 'column', my: 5 }}>
-            <FormControlLabel
-              id='PayPal'
-              value='PayPal'
-              checked={paymentMethod === 'PayPal'}
-              onClick={(e) => setPaymentMethod(e.target.value)}
-              control={<Radio />}
-              label=' PayPal or Credit Card'
-            />
-            <FormControlLabel
-              id='stripe'
-              value={stripe}
-              onClick={(e) => setStripe(e.target.value)}
-              control={<Radio />}
-              label='Stripe'
-            />
-          </Box>
-        </RadioGroup>
-
-        <Button
-          type='submit'
-          fullWidth
-          variant='contained'
-          sx={{ mt: 3, mb: 3 }}>
-          Continue
-        </Button>
+        <Box
+          component='form'
+          onSubmit={submitHandler}
+          noValidate
+          sx={{ mt: 1 }}>
+          <RadioGroup name='use-radio-group' defaultValue='first'>
+            <Box sx={{ display: 'flex', flexDirection: 'column', my: 5 }}>
+              <FormControlLabel
+                id='PayPal'
+                value='PayPal'
+                checked={paymentMethod === 'PayPal'}
+                onClick={(e) => setPaymentMethod(e.target.value)}
+                control={<Radio />}
+                label=' PayPal or Credit Card'
+              />
+              <FormControlLabel
+                id='stripe'
+                value={stripe}
+                onClick={(e) => setStripe(e.target.value)}
+                control={<Radio />}
+                label='Stripe'
+              />
+            </Box>
+          </RadioGroup>
+          <Button
+            type='submit'
+            fullWidth
+            variant='contained'
+            sx={{ mt: 3, mb: 3 }}>
+            Continue
+          </Button>
+        </Box>
       </Box>
     </FromContainer>
   )
