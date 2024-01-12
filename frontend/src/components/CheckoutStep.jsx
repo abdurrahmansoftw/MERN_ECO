cdc188508526b94666a52de1734100d87dbfe4fb
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2024-01-12 09:24:17 +0600

    CheckoutStep

--- frontend/src/components/CheckoutStep.jsx
+++ frontend/src/components/CheckoutStep.jsx
@@ -1,15 +1,27 @@
-import { Box } from '@mui/material'
+import { Box, Button, Step, StepLabel, Stepper } from '@mui/material'
 import React from 'react'
-import CustomLink from './CustomLink'
+import { Link } from 'react-router-dom'
 
 const CheckoutStep = ({ stepOne, stepTwo, stepThree, stepFour }) => {
   return (
     <Box>
-      {stepOne ? (
-        <CustomLink to='/login'>Sign In</CustomLink>
-      ) : (
-        <CustomLink disabled>Sign In</CustomLink>
-      )}
+      <Stepper activeStep={1} alternativeLabel>
+        {stepOne ? (
+          <Step>
+            <StepLabel>
+              <Button component={Link} to='/login'>
+                Sign In
+              </Button>
+            </StepLabel>
+          </Step>
+        ) : (
+          <Step>
+            <StepLabel>
+              <Button disabled>Sign In</Button>
+            </StepLabel>
+          </Step>
+        )}
+      </Stepper>
     </Box>
   )
 }
