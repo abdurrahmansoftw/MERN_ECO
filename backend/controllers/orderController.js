5598b939985854a1e2f108263284e276cacd5594
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2024-01-13 09:11:37 +0600

    getMyOrders

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -6,12 +6,57 @@ import { calcPrices } from '../utils/calcPrices.js'
 // @docs    Create new order
 // @route   POST /api/orders
 // @access  Private
+const addOrderItems = asyncHandler(async (req, res) => {
+  const { orderItems, shippingAddress, paymentMethod } = req.body
+
+  if (orderItems && orderItems.length === 0) {
+    res.status(400)
+    throw new Error('No order items')
+  } else {
+    const itemsFromDB = await Product.find({
+      _id: { $in: orderItems.map((x) => x._id) },
+    })
+
+    // map over the order items and use the price from our items from database
+    const dbOrderItems = orderItems.map((itemFromClient) => {
+      const matchingItemFromDB = itemsFromDB.find(
+        (itemFromDB) => itemFromDB._id.toString() === itemFromClient._id
+      )
+      return {
+        ...itemFromClient,
+        product: itemFromClient._id,
+        price: matchingItemFromDB.price,
+        _id: undefined,
+      }
+    })
+
+    // calculate prices
+    const { itemsPrice, taxPrice, shippingPrice, totalPrice } =
+      calcPrices(dbOrderItems)
+
+    const order = new Order({
+      orderItems: dbOrderItems,
+      user: req.user._id,
+      shippingAddress,
+      paymentMethod,
+      itemsPrice,
+      taxPrice,
+      shippingPrice,
+      totalPrice,
+    })
+
+    const createdOrder = await order.save()
+
+    res.status(201).json(createdOrder)
+  }
+})
 
 // @docs    Get Logged in user orders
 // @route   GET /api/orders/myorders
 // @access  Private
 const getMyOrders = asyncHandler(async (req, res) => {
-  res.send('get My Order Items')
+  const orders = await Order.find({ user: req.user._id })
+  res.status(200).json(orders)
 })
 
 // @docs    Get order by ID
