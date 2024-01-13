acc163981c5a98ee947eda228d0911e15b678b1b
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2024-01-13 09:22:17 +0600

    ordersApiSlice

---
+++ frontend/src/slices/ordersApiSlice.js
@@ -0,0 +1,15 @@
+import { apiSlice } from './apiSlice'
+
+export const ordersApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    addOrderItems: builder.mutation({
+      query: (order) => ({
+        url: '/orders',
+        method: 'POST',
+        body: order,
+      }),
+    }),
+  }),
+})
+
+export const { useAddOrderItemsMutation } = ordersApiSlice
