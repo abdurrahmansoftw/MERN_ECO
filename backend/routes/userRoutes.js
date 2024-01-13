0bcfde614eb4003b14ffc57c3abd39c94c172cc3
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2024-01-13 08:07:47 +0600

    user model

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -17,7 +17,13 @@ const router = express.Router()
 router.route('/').post(registerUser).get(protect, admin, getUsers)
 router.post('/auth', authUser)
 router.post('/logout', logoutUser)
-router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile)
-router.route('/:id').delete(protect, admin, deleteUser).get(protect, admin, getUserById).put(protect, admin, updateUser)
-
+router
+  .route('/profile')
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile)
+router
+  .route('/:id')
+  .delete(protect, admin, deleteUser)
+  .get(protect, admin, getUserById)
+  .put(protect, admin, updateUser)
 export default router
