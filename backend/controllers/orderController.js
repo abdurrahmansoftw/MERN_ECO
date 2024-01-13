624169fed2a43708081807b121a17f82626e08ff
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2024-01-13 07:47:52 +0600

    getMyOders

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -6,3 +6,10 @@ import asyncHandler from '../middleware/asyncHandler.js'
 const addOrderItems = asyncHandler(async (req, res) => {
   res.send('Order created')
 })
+
+// @docs    Get Logged in user orders
+// @route   GET /api/orders/myorders
+// @access  Private
+const getMyOders = asyncHandler(async (req, res) => {
+  res.send('get My Order Items')
+})
