3cb89f711ef8a5a0970f08d3a6a32fa4ff65e207
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2024-01-13 09:15:10 +0600

    if order is emapty

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,52 +1,35 @@
 import asyncHandler from '../middleware/asyncHandler.js'
 import Order from '../models/orderModel.js'
-import Product from '../models/productModel.js'
-import { calcPrices } from '../utils/calcPrices.js'
 
 // @docs    Create new order
 // @route   POST /api/orders
 // @access  Private
 const addOrderItems = asyncHandler(async (req, res) => {
-  const { orderItems, shippingAddress, paymentMethod } = req.body
+  const {
+    orderItems,
+    shippingAddress,
+    paymentMethod,
+    shippingPrice,
+    itemsPrice,
+    totalPrice,
+  } = req.body
 
   if (orderItems && orderItems.length === 0) {
     res.status(400)
     throw new Error('No order items')
+    return
   } else {
-    const itemsFromDB = await Product.find({
-      _id: { $in: orderItems.map((x) => x._id) },
-    })
-
-    // map over the order items and use the price from our items from database
-    const dbOrderItems = orderItems.map((itemFromClient) => {
-      const matchingItemFromDB = itemsFromDB.find(
-        (itemFromDB) => itemFromDB._id.toString() === itemFromClient._id
-      )
-      return {
-        ...itemFromClient,
-        product: itemFromClient._id,
-        price: matchingItemFromDB.price,
-        _id: undefined,
-      }
-    })
-
-    // calculate prices
-    const { itemsPrice, taxPrice, shippingPrice, totalPrice } =
-      calcPrices(dbOrderItems)
-
     const order = new Order({
-      orderItems: dbOrderItems,
+      orderItems,
       user: req.user._id,
       shippingAddress,
       paymentMethod,
-      itemsPrice,
-      taxPrice,
       shippingPrice,
+      itemsPrice,
       totalPrice,
     })
 
     const createdOrder = await order.save()
-
     res.status(201).json(createdOrder)
   }
 })
