d1f060f53d44a72e445322ae3dfb11a2913a816a
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2024-01-15 10:49:35 +0600

    update

--- backend/database/database.js
+++ backend/database/database.js
@@ -2,13 +2,13 @@ import colors from 'colors'
 import mongoose from 'mongoose'
 
 const connectDB = async () => {
-  try {
-    const conn = await mongoose.connect(process.env.MONGO_URI)
-    console.log(colors.rainbow(`MongoDB Connected: ${conn.connection.host}`))
-  } catch (error) {
-    console.error(`Error: ${error.message}`)
-    process.exit(1)
-  }
+	try {
+		const conn = await mongoose.connect(process.env.MONGO_URI)
+		console.log(colors.rainbow(`MongoDB Connected: ${conn.connection.host}`))
+	} catch (error) {
+		console.error(`Error: ${error.message}`)
+		process.exit(1)
+	}
 }
 
 export default connectDB
