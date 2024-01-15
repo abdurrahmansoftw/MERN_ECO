a8030e0cbe61a8d7426e6dbe905b1244fcb80b18
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2024-01-15 11:09:09 +0600

    users data update

--- backend/data/users.js
+++ backend/data/users.js
@@ -1,23 +1,22 @@
 import bcrypt from 'bcryptjs'
 
 const users = [
-  {
-    name: 'AbdurRahman',
-    email: 'admin@email.com',
-    password: bcrypt.hashSync('123456', 10),
-    isAdmin: true,
-  },
-  {
-    name: 'akaid',
-    email: 'akaid@email.com',
-    password: bcrypt.hashSync('123456', 10),
-    isAdmin: true,
-  },
-  {
-    name: 'Abdur Rahman',
-    email: 'user@email.com',
-    password: bcrypt.hashSync('123456', 10),
-  },
+	{
+		name: 'AbdurRahman',
+		email: 'admin@email.com',
+		password: bcrypt.hashSync('123456', 10),
+		isAdmin: true,
+	},
+	{
+		name: 'akaid',
+		email: 'akaid@email.com',
+		password: bcrypt.hashSync('123456', 10),
+	},
+	{
+		name: 'Abdur Rahman',
+		email: 'user@email.com',
+		password: bcrypt.hashSync('123456', 10),
+	},
 ]
 
 export default users
