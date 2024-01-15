fd6a118bfc6307ca469845ff82ec02c6371c73b3
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2024-01-15 10:53:16 +0600

    update

--- frontend/src/components/FromContainer.jsx
+++ frontend/src/components/FromContainer.jsx
@@ -2,12 +2,12 @@ import { Container, CssBaseline } from '@mui/material'
 import React from 'react'
 
 const FromContainer = ({ children }) => {
-  return (
-    <Container component='main' maxWidth='sm'>
-      <CssBaseline />
-      {children}
-    </Container>
-  )
+	return (
+		<Container component='main' maxWidth='sm'>
+			<CssBaseline />
+			{children}
+		</Container>
+	)
 }
 
 export default FromContainer
--- frontend/src/components/Rating.jsx
+++ frontend/src/components/Rating.jsx
@@ -5,61 +5,61 @@ import StarHalfIcon from '@mui/icons-material/StarHalf'
 import { Box } from '@mui/material'
 
 const Rating = ({ value, text }) => {
-  return (
-    <Box>
-      <span>
-        {value >= 1 ? (
-          <StarIcon fontSize='small' color='warning' />
-        ) : value >= 0.5 ? (
-          <StarHalfIcon fontSize='small' color='warning' />
-        ) : (
-          <StarBorderIcon fontSize='small' color='warning' />
-        )}
-      </span>
+	return (
+		<Box>
+			<span>
+				{value >= 1 ? (
+					<StarIcon fontSize='small' color='warning' />
+				) : value >= 0.5 ? (
+					<StarHalfIcon fontSize='small' color='warning' />
+				) : (
+					<StarBorderIcon fontSize='small' color='warning' />
+				)}
+			</span>
 
-      <span>
-        {value >= 2 ? (
-          <StarIcon fontSize='small' color='warning' />
-        ) : value >= 1.5 ? (
-          <StarHalfIcon fontSize='small' color='warning' />
-        ) : (
-          <StarBorderIcon fontSize='small' color='warning' />
-        )}
-      </span>
+			<span>
+				{value >= 2 ? (
+					<StarIcon fontSize='small' color='warning' />
+				) : value >= 1.5 ? (
+					<StarHalfIcon fontSize='small' color='warning' />
+				) : (
+					<StarBorderIcon fontSize='small' color='warning' />
+				)}
+			</span>
 
-      <span>
-        {value >= 3 ? (
-          <StarIcon fontSize='small' color='warning' />
-        ) : value >= 2.5 ? (
-          <StarHalfIcon fontSize='small' color='warning' />
-        ) : (
-          <StarBorderIcon fontSize='small' color='warning' />
-        )}
-      </span>
+			<span>
+				{value >= 3 ? (
+					<StarIcon fontSize='small' color='warning' />
+				) : value >= 2.5 ? (
+					<StarHalfIcon fontSize='small' color='warning' />
+				) : (
+					<StarBorderIcon fontSize='small' color='warning' />
+				)}
+			</span>
 
-      <span>
-        {value >= 4 ? (
-          <StarIcon fontSize='small' color='warning' />
-        ) : value >= 3.5 ? (
-          <StarHalfIcon fontSize='small' color='warning' />
-        ) : (
-          <StarBorderIcon fontSize='small' color='warning' />
-        )}
-      </span>
+			<span>
+				{value >= 4 ? (
+					<StarIcon fontSize='small' color='warning' />
+				) : value >= 3.5 ? (
+					<StarHalfIcon fontSize='small' color='warning' />
+				) : (
+					<StarBorderIcon fontSize='small' color='warning' />
+				)}
+			</span>
 
-      <span>
-        {value >= 5 ? (
-          <StarIcon fontSize='small' color='warning' />
-        ) : value >= 4.5 ? (
-          <StarHalfIcon fontSize='small' color='warning' />
-        ) : (
-          <StarBorderIcon fontSize='small' color='warning' />
-        )}
-      </span>
+			<span>
+				{value >= 5 ? (
+					<StarIcon fontSize='small' color='warning' />
+				) : value >= 4.5 ? (
+					<StarHalfIcon fontSize='small' color='warning' />
+				) : (
+					<StarBorderIcon fontSize='small' color='warning' />
+				)}
+			</span>
 
-      <span className='text-nowrap'> {text && text} </span>
-    </Box>
-  )
+			<span className='text-nowrap'> {text && text} </span>
+		</Box>
+	)
 }
 
 export default Rating
