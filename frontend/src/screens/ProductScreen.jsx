85b7ccc353e6de7586fc0d2cccc1a0cc902fa7ae
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2024-01-15 10:55:35 +0600

    update

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,17 +1,17 @@
 import {
-  Button,
-  CardMedia,
-  Divider,
-  FormControl,
-  Grid,
-  List,
-  ListItem,
-  ListItemButton,
-  ListItemText,
-  MenuItem,
-  Paper,
-  Select,
-  Typography,
+	Button,
+	CardMedia,
+	Divider,
+	FormControl,
+	Grid,
+	List,
+	ListItem,
+	ListItemButton,
+	ListItemText,
+	MenuItem,
+	Paper,
+	Select,
+	Typography,
 } from '@mui/material'
 import { useState } from 'react'
 import { useDispatch } from 'react-redux'
@@ -21,153 +21,155 @@ import { addToCart } from '../slices/cartSlice'
 import { useGetProductDetailsQuery } from '../slices/productsApiSlice'
 
 const ProductScreen = () => {
-  const { id: productId } = useParams()
-  const dispatch = useDispatch()
-  const navigate = useNavigate()
+	const { id: productId } = useParams()
+	const dispatch = useDispatch()
+	const navigate = useNavigate()
 
-  const [qty, setQty] = useState(1)
+	const [qty, setQty] = useState(1)
 
-  const {
-    data: product,
-    isLoading,
-    error,
-  } = useGetProductDetailsQuery(productId)
+	const {
+		data: product,
+		isLoading,
+		error,
+	} = useGetProductDetailsQuery(productId)
 
-  const addTocardHandler = () => {
-    dispatch(addToCart({ ...product, qty }))
-    navigate('/cart')
-  }
+	const addTocardHandler = () => {
+		dispatch(addToCart({ ...product, qty }))
+		navigate('/cart')
+	}
 
-  if (isLoading) return <div>Loading...</div>
-  if (error) return <div>{error.status}</div>
+	if (isLoading) return <div>Loading...</div>
+	if (error) return <div>{error.status}</div>
 
-  return (
-    <Grid container spacing={3} sx={{ my: 5 }}>
-      <Grid item xs={12} md={5}>
-        <CardMedia
-          sx={{ height: 400 }}
-          image={product.image}
-          title={product.name}
-        />
-      </Grid>
-      <Grid item xs={12} md={4}>
-        <Paper aria-label='main mailbox folders' elevation={0}>
-          <List>
-            <ListItem disablePadding>
-              <ListItemButton>
-                <ListItemText>
-                  <Typography variant='h5' component='h1'>
-                    {product.name}
-                  </Typography>
-                </ListItemText>
-              </ListItemButton>
-            </ListItem>
-            <Divider />
-            <ListItem disablePadding>
-              <ListItemButton>
-                <ListItemText primary={product.description} />
-              </ListItemButton>
-            </ListItem>
-            <Divider />
-            <ListItem disablePadding>
-              <ListItemButton>
-                <ListItemText primary={product.brand} />
-              </ListItemButton>
-            </ListItem>
-            <Divider />
-            <ListItem disablePadding>
-              <ListItemButton>
-                <ListItemText primary={product.category} />
-              </ListItemButton>
-            </ListItem>
+	return (
+		<Grid container spacing={3} sx={{ my: 5 }}>
+			<Grid item xs={12} md={5}>
+				<CardMedia
+					sx={{ height: 400 }}
+					image={product.image}
+					title={product.name}
+				/>
+			</Grid>
+			<Grid item xs={12} md={4}>
+				<Paper aria-label='main mailbox folders' elevation={0}>
+					<List>
+						<ListItem disablePadding>
+							<ListItemButton>
+								<ListItemText>
+									<Typography variant='h5' component='h1'>
+										{product.name}
+									</Typography>
+								</ListItemText>
+							</ListItemButton>
+						</ListItem>
+						<Divider />
+						<ListItem disablePadding>
+							<ListItemButton>
+								<ListItemText primary={product.description} />
+							</ListItemButton>
+						</ListItem>
+						<Divider />
+						<ListItem disablePadding>
+							<ListItemButton>
+								<ListItemText primary={product.brand} />
+							</ListItemButton>
+						</ListItem>
+						<Divider />
+						<ListItem disablePadding>
+							<ListItemButton>
+								<ListItemText primary={product.category} />
+							</ListItemButton>
+						</ListItem>
 
-            <Divider />
-            <ListItem disablePadding>
-              <ListItemButton>
-                <Rating
-                  value={product.rating}
-                  text={`${product.numReviews} reviews`}
-                />
-              </ListItemButton>
-            </ListItem>
-          </List>
-        </Paper>
-        <Divider />
-      </Grid>
-      <Grid item xs={12} md={3}>
-        <Paper aria-label='main mailbox folders' elevation={0}>
-          <List sx={{ display: 'flex', justifyContent: 'space-between' }}>
-            <ListItem disablePadding>
-              <ListItemButton>
-                <ListItemText primary='Price' />
-              </ListItemButton>
-            </ListItem>
-            <Divider />
-            <ListItem disablePadding>
-              <ListItemButton>
-                <ListItemText> ${product.price}</ListItemText>
-              </ListItemButton>
-            </ListItem>
-          </List>
-          <Divider />
-          <List sx={{ display: 'flex', justifyContent: 'space-between' }}>
-            <ListItem disablePadding>
-              <ListItemButton>
-                <ListItemText primary='Status' />
-              </ListItemButton>
-            </ListItem>
-            <Divider />
-            <ListItem disablePadding>
-              <ListItemButton>
-                <ListItemText>
-                  {product.countInStock > 0 ? 'In Stock' : 'Out of Stock'}
-                </ListItemText>
-              </ListItemButton>
-            </ListItem>
-          </List>
-          <Divider />
-          {product.countInStock > 0 && (
-            <List sx={{ display: 'flex', justifyContent: 'space-between' }}>
-              <ListItem disablePadding>
-                <ListItemButton>
-                  <ListItemText primary='Qty' />
-                </ListItemButton>
-              </ListItem>
-              <Divider />
-              <ListItem disablePadding>
-                <ListItemButton>
-                  <ListItemText>
-                    <FormControl fullWidth size='small' variant='standard'>
-                      <Select
-                        value={qty}
-                        onChange={(e) => setQty(e.target.value)}>
-                        {[...Array(product.countInStock).keys()].map((x) => (
-                          <MenuItem key={x + 1} value={x + 1}>
-                            {x + 1}
-                          </MenuItem>
-                        ))}
-                      </Select>
-                    </FormControl>
-                  </ListItemText>
-                </ListItemButton>
-              </ListItem>
-            </List>
-          )}
+						<Divider />
+						<ListItem disablePadding>
+							<ListItemButton>
+								<Rating
+									value={product.rating}
+									text={`${product.numReviews} reviews`}
+								/>
+							</ListItemButton>
+						</ListItem>
+					</List>
+				</Paper>
+				<Divider />
+			</Grid>
+			<Grid item xs={12} md={3}>
+				<Paper aria-label='main mailbox folders' elevation={0}>
+					<List sx={{ display: 'flex', justifyContent: 'space-between' }}>
+						<ListItem disablePadding>
+							<ListItemButton>
+								<ListItemText primary='Price' />
+							</ListItemButton>
+						</ListItem>
+						<Divider />
+						<ListItem disablePadding>
+							<ListItemButton>
+								<ListItemText> ${product.price}</ListItemText>
+							</ListItemButton>
+						</ListItem>
+					</List>
+					<Divider />
+					<List sx={{ display: 'flex', justifyContent: 'space-between' }}>
+						<ListItem disablePadding>
+							<ListItemButton>
+								<ListItemText primary='Status' />
+							</ListItemButton>
+						</ListItem>
+						<Divider />
+						<ListItem disablePadding>
+							<ListItemButton>
+								<ListItemText>
+									{product.countInStock > 0 ? 'In Stock' : 'Out of Stock'}
+								</ListItemText>
+							</ListItemButton>
+						</ListItem>
+					</List>
+					<Divider />
+					{product.countInStock > 0 && (
+						<List sx={{ display: 'flex', justifyContent: 'space-between' }}>
+							<ListItem disablePadding>
+								<ListItemButton>
+									<ListItemText primary='Qty' />
+								</ListItemButton>
+							</ListItem>
+							<Divider />
+							<ListItem disablePadding>
+								<ListItemButton>
+									<ListItemText>
+										<FormControl fullWidth size='small' variant='standard'>
+											<Select
+												value={qty}
+												onChange={(e) => setQty(e.target.value)}
+											>
+												{[...Array(product.countInStock).keys()].map((x) => (
+													<MenuItem key={x + 1} value={x + 1}>
+														{x + 1}
+													</MenuItem>
+												))}
+											</Select>
+										</FormControl>
+									</ListItemText>
+								</ListItemButton>
+							</ListItem>
+						</List>
+					)}
 
-          <Button
-            onClick={addTocardHandler}
-            elevation={0}
-            disabled={product.countInStock === 0}
-            variant='contained'
-            color='info'
-            sx={{ display: 'block', width: '100%' }}>
-            Add to Cart
-          </Button>
-        </Paper>
-        <Divider />
-      </Grid>
-    </Grid>
-  )
+					<Button
+						onClick={addTocardHandler}
+						elevation={0}
+						disabled={product.countInStock === 0}
+						variant='contained'
+						color='info'
+						sx={{ display: 'block', width: '100%' }}
+					>
+						Add to Cart
+					</Button>
+				</Paper>
+				<Divider />
+			</Grid>
+		</Grid>
+	)
 }
 
 export default ProductScreen
--- frontend/src/screens/ShippingScreen.jsx
+++ frontend/src/screens/ShippingScreen.jsx
@@ -9,103 +9,106 @@ import FromContainer from '../components/FromContainer'
 import { saveShippingAddress } from '../slices/cartSlice'
 
 const ShippingScreen = () => {
-  const cart = useSelector((state) => state.cart)
-  const { shippingAddress } = cart
+	const cart = useSelector((state) => state.cart)
+	const { shippingAddress } = cart
 
-  const [address, setAddress] = useState(shippingAddress?.address || '')
-  const [city, setCity] = useState(shippingAddress?.city || '')
-  const [postalCode, setPostalCode] = useState(
-    shippingAddress?.postalCode || ''
-  )
-  const [country, setCountry] = useState(shippingAddress?.country || '')
+	const [address, setAddress] = useState(shippingAddress?.address || '')
+	const [city, setCity] = useState(shippingAddress?.city || '')
+	const [postalCode, setPostalCode] = useState(
+		shippingAddress?.postalCode || ''
+	)
+	const [country, setCountry] = useState(shippingAddress?.country || '')
 
-  const dispatch = useDispatch()
-  const navigate = useNavigate()
+	const dispatch = useDispatch()
+	const navigate = useNavigate()
 
-  const submitHandler = (e) => {
-    e.preventDefault()
-    dispatch(saveShippingAddress({ address, city, postalCode, country }))
-    navigate('/payment')
-  }
+	const submitHandler = (e) => {
+		e.preventDefault()
+		dispatch(saveShippingAddress({ address, city, postalCode, country }))
+		navigate('/payment')
+	}
 
-  return (
-    <FromContainer>
-      <Box
-        sx={{
-          marginTop: 8,
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-        }}>
-        <Avatar sx={{ m: 1, bgcolor: 'primary.main' }}>
-          <LocalShippingIcon />
-        </Avatar>
-        <Typography component='h1' variant='h4'>
-          Shipping address
-        </Typography>
+	return (
+		<FromContainer>
+			<Box
+				sx={{
+					marginTop: 8,
+					display: 'flex',
+					flexDirection: 'column',
+					alignItems: 'center',
+				}}
+			>
+				<Avatar sx={{ m: 1, bgcolor: 'primary.main' }}>
+					<LocalShippingIcon />
+				</Avatar>
+				<Typography component='h1' variant='h4'>
+					Shipping address
+				</Typography>
 
-        <CheckoutStep stepOne stepTwo />
+				<CheckoutStep stepOne stepTwo />
 
-        <Box
-          component='form'
-          onSubmit={submitHandler}
-          noValidate
-          sx={{ mt: 1 }}>
-          <TextField
-            margin='normal'
-            required
-            fullWidth
-            label='Address'
-            type='text'
-            value={address}
-            onChange={(e) => setAddress(e.target.value)}
-            autoComplete='address'
-            autoFocus
-          />
-          <TextField
-            margin='normal'
-            required
-            fullWidth
-            value={city}
-            onChange={(e) => setCity(e.target.value)}
-            label='City'
-            type='text'
-            autoComplete='current-city'
-          />
-          <TextField
-            margin='normal'
-            required
-            fullWidth
-            label='Postal Code'
-            type='number'
-            value={postalCode}
-            onChange={(e) => setPostalCode(e.target.value)}
-            autoComplete='number'
-          />
-          <TextField
-            margin='normal'
-            required
-            fullWidth
-            name='country'
-            value={country}
-            onChange={(e) => setCountry(e.target.value)}
-            label='country'
-            type='text'
-            id='country'
-            autoComplete='current-country'
-          />
+				<Box
+					component='form'
+					onSubmit={submitHandler}
+					noValidate
+					sx={{ mt: 1 }}
+				>
+					<TextField
+						margin='normal'
+						required
+						fullWidth
+						label='Address'
+						type='text'
+						value={address}
+						onChange={(e) => setAddress(e.target.value)}
+						autoComplete='address'
+						autoFocus
+					/>
+					<TextField
+						margin='normal'
+						required
+						fullWidth
+						value={city}
+						onChange={(e) => setCity(e.target.value)}
+						label='City'
+						type='text'
+						autoComplete='current-city'
+					/>
+					<TextField
+						margin='normal'
+						required
+						fullWidth
+						label='Postal Code'
+						type='number'
+						value={postalCode}
+						onChange={(e) => setPostalCode(e.target.value)}
+						autoComplete='number'
+					/>
+					<TextField
+						margin='normal'
+						required
+						fullWidth
+						name='country'
+						value={country}
+						onChange={(e) => setCountry(e.target.value)}
+						label='country'
+						type='text'
+						id='country'
+						autoComplete='current-country'
+					/>
 
-          <Button
-            type='submit'
-            fullWidth
-            variant='contained'
-            sx={{ mt: 3, mb: 3 }}>
-            Continue
-          </Button>
-        </Box>
-      </Box>
-    </FromContainer>
-  )
+					<Button
+						type='submit'
+						fullWidth
+						variant='contained'
+						sx={{ mt: 3, mb: 3 }}
+					>
+						Continue
+					</Button>
+				</Box>
+			</Box>
+		</FromContainer>
+	)
 }
 
 export default ShippingScreen
