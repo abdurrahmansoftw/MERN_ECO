3343cf53d40bb8f5aaa1b411ec01fc5280f9ba06
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2024-01-15 10:54:07 +0600

    update

--- frontend/src/routes/router.jsx
+++ frontend/src/routes/router.jsx
@@ -14,29 +14,29 @@ import ShippingScreen from '../screens/ShippingScreen'
 import PrivateRoutes from './PrivateRoutes'
 
 const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Layout />,
-    errorElement: <ErrorPage />,
-    children: [
-      { index: true, element: <HomeScreen /> },
-      { path: 'product/:id', element: <ProductScreen /> },
-      { path: 'cart', element: <CartScreen /> },
-      { path: 'login', element: <LoginScreen /> },
-      { path: 'register', element: <RegisterScreen /> },
-      { path: 'forgotpassword', element: <ForgotPasswordScreen /> },
-    ],
-  },
-  /* Private routes */
-  {
-    element: <PrivateRoutes />,
-    children: [
-      { path: 'shipping', element: <ShippingScreen /> },
-      { path: 'payment', element: <PaymentScreen /> },
-      { path: 'placeorder', element: <PlaceOrderScreen /> },
-      { path: 'order/:id', element: <OrderScreen /> },
-    ],
-  },
+	{
+		path: '/',
+		element: <Layout />,
+		errorElement: <ErrorPage />,
+		children: [
+			{ index: true, element: <HomeScreen /> },
+			{ path: 'product/:id', element: <ProductScreen /> },
+			{ path: 'cart', element: <CartScreen /> },
+			{ path: 'login', element: <LoginScreen /> },
+			{ path: 'register', element: <RegisterScreen /> },
+			{ path: 'forgotpassword', element: <ForgotPasswordScreen /> },
+		],
+	},
+	/* Private routes */
+	{
+		element: <PrivateRoutes />,
+		children: [
+			{ path: 'shipping', element: <ShippingScreen /> },
+			{ path: 'payment', element: <PaymentScreen /> },
+			{ path: 'placeorder', element: <PlaceOrderScreen /> },
+			{ path: 'order/:id', element: <OrderScreen /> },
+		],
+	},
 ])
 
 export default router
