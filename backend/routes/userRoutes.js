e81df3ac7b06b29e8e11bd7936686b45cb5f99a0
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2024-01-16 10:25:58 +0600

    router

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,14 +1,14 @@
 import express from 'express'
 import {
-  authUser,
-  deleteUser,
-  getUserById,
-  getUserProfile,
-  getUsers,
-  logoutUser,
-  registerUser,
-  updateUser,
-  updateUserProfile,
+	authUser,
+	deleteUser,
+	getUserById,
+	getUserProfile,
+	getUsers,
+	logoutUser,
+	registerUser,
+	updateUser,
+	updateUserProfile,
 } from '../controllers/userController.js'
 import { admin, protect } from '../middleware/authMiddleware.js'
 
@@ -18,12 +18,12 @@ router.route('/').post(registerUser).get(protect, admin, getUsers)
 router.post('/auth', authUser)
 router.post('/logout', logoutUser)
 router
-  .route('/profile')
-  .get(protect, getUserProfile)
-  .put(protect, updateUserProfile)
+	.route('/profile')
+	.get(protect, getUserProfile)
+	.put(protect, updateUserProfile)
 router
-  .route('/:id')
-  .delete(protect, admin, deleteUser)
-  .get(protect, admin, getUserById)
-  .put(protect, admin, updateUser)
+	.route('/:id')
+	.delete(protect, admin, deleteUser)
+	.get(protect, admin, getUserById)
+	.put(protect, admin, updateUser)
 export default router
