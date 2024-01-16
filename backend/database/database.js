b42ac056df5a6b0236a96e7feaa4e4fe78d82922
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2024-01-16 10:24:44 +0600

    Database

--- backend/database/database.js
+++ backend/database/database.js
@@ -4,7 +4,9 @@ import mongoose from 'mongoose'
 const connectDB = async () => {
 	try {
 		const conn = await mongoose.connect(process.env.MONGO_URI)
-		console.log(colors.rainbow(`MongoDB Connected: ${conn.connection.host}`))
+		console.log(
+			colors.rainbow(`MongoDB Database Connected: ${conn.connection.host}`)
+		)
 	} catch (error) {
 		console.error(`Error: ${error.message}`)
 		process.exit(1)
