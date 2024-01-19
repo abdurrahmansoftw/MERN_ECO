ceeccb122ee8733f36be0e753d1f71fa797e5295
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2024-01-19 09:57:12 +0600

    protect

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -5,14 +5,12 @@ import asyncHandler from './asyncHandler.js'
 // User must be authenticated
 const protect = asyncHandler(async (req, res, next) => {
 	let token
-
 	// Read JWT from the 'jwt' cookie
 	token = req.cookies.jwt
 
 	if (token) {
 		try {
 			const decoded = jwt.verify(token, process.env.JWT_SECRET)
-
 			req.user = await User.findById(decoded.userId).select('-password')
 
 			next()
