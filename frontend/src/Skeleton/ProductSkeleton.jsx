ea05c830fbbf9d80f5cac0cb24f142b5ee2f7c8f
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2024-01-19 09:54:16 +0600

    ProductSkeleton

--- frontend/src/Skeleton/ProductSkeleton.jsx
+++ frontend/src/Skeleton/ProductSkeleton.jsx
@@ -2,13 +2,13 @@ import { Grid, Skeleton } from '@mui/material'
 import React from 'react'
 
 const ProductSkeleton = () => {
-  return (
-    <Grid container spacing={2}>
-      <Grid item xs={12} sm={6} md={4} lg={4} xl={3}>
-        <Skeleton sx={{ bgcolor: 'grey.900' }} />
-      </Grid>
-    </Grid>
-  )
+	return (
+		<Grid container spacing={2}>
+			<Grid item xs={12} sm={6} md={4} lg={4} xl={3}>
+				<Skeleton sx={{ bgcolor: 'grey.900' }} />
+			</Grid>
+		</Grid>
+	)
 }
 
 export default ProductSkeleton
