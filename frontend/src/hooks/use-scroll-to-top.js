0d7e409c9e5f32b1f27321948bfb87363a5259fb
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2024-01-19 09:53:55 +0600

    useScrollToTop

--- frontend/src/hooks/use-scroll-to-top.js
+++ frontend/src/hooks/use-scroll-to-top.js
@@ -1,12 +1,8 @@
 import { useEffect } from 'react'
 import { useLocation } from 'react-router-dom'
 
-// ----------------------------------------------------------------------
-
 export function useScrollToTop() {
 	const { pathname } = useLocation()
-
-	// biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
 	useEffect(() => {
 		window.scrollTo(0, 0)
 	}, [pathname])
