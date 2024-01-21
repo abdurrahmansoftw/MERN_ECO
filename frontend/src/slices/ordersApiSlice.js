d4311e689424e0fe1519c1e895d246f3c139f181
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2024-01-21 19:00:50 +0600

    payOrder

--- frontend/src/slices/ordersApiSlice.js
+++ frontend/src/slices/ordersApiSlice.js
@@ -16,6 +16,13 @@ export const ordersApiSlice = apiSlice.injectEndpoints({
 			}),
 			keepUnusedDataFor: 5,
 		}),
+		payOrder: builder.mutation({
+			query: (orderId, details) => ({
+				url: `${ORDERS_URL}/${orderId}/pay`,
+				method: 'PUT',
+				body: { ...details },
+			}),
+		}),
 	}),
 })
 
