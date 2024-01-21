9d5a92fbea9005afdeb5c7cc5539d78dbd841ba0
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2024-01-21 15:48:35 +0600

    useEffect

--- frontend/src/screens/PlaceOrderScreen.jsx
+++ frontend/src/screens/PlaceOrderScreen.jsx
@@ -1,10 +1,18 @@
 import { Box } from '@mui/material'
+import { useEffect } from 'react'
 import { useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 
 const PlaceOrderScreen = () => {
 	const navigate = useNavigate()
 	const cart = useSelector((state) => state.cart)
+	
+	useEffect(() => {
+		if (!cart.shippingAddress) {
+			navigate('/shipping')
+		}
+	}, [cart.shippingAddress, navigate])
+
 	return <Box>Ok</Box>
 }
 
