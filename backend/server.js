09ce44e6740019542bd13cbff3487f8526aa1f92
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2024-01-21 18:53:46 +0600

    paypal route

--- backend/server.js
+++ backend/server.js
@@ -30,6 +30,11 @@ app.use('/api/users', userRoutes)
 app.use('/api/products', productRoutes)
 app.use('/api/orders', orderRoutes)
 
+// paypal route
+app.get('/api/config/paypal', (req, res) =>
+	res.send(process.env.PAYPAL_CLIENT_ID)
+)
+
 // error middleware
 app.use(notFound)
 app.use(errorHandler)
