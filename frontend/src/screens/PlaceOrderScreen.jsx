abf57fe4d0c1a2b21c715348dae058f938f3b9d0
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2024-01-21 15:48:20 +0600

    navigate

--- frontend/src/screens/PlaceOrderScreen.jsx
+++ frontend/src/screens/PlaceOrderScreen.jsx
@@ -1,7 +1,9 @@
 import { Box } from '@mui/material'
 import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
 
 const PlaceOrderScreen = () => {
+	const navigate = useNavigate()
 	const cart = useSelector((state) => state.cart)
 	return <Box>Ok</Box>
 }
