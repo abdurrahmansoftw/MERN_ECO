27c6b730097b3c874cc0a4b1fc9f53dd45d59ec6
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2024-01-21 15:48:04 +0600

    cart

--- frontend/src/screens/PlaceOrderScreen.jsx
+++ frontend/src/screens/PlaceOrderScreen.jsx
@@ -1,6 +1,8 @@
 import { Box } from '@mui/material'
+import { useSelector } from 'react-redux'
 
 const PlaceOrderScreen = () => {
+	const cart = useSelector((state) => state.cart)
 	return <Box>Ok</Box>
 }
 
