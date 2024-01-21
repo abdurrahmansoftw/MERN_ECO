0bb6ce7d47b072e2bdfdd5e01fe97bff73326a3f
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2024-01-21 18:15:57 +0600

    OrderScreen

--- frontend/src/screens/OrderScreen.jsx
+++ frontend/src/screens/OrderScreen.jsx
@@ -1,6 +1,16 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
+import { useGetOrderDetailsQuery } from '../slices/ordersApiSlice'
 
 const OrderScreen = () => {
+	const { id: orderId } = useParams()
+	const {
+		data: order,
+		refetch,
+		isLoading,
+		error,
+	} = useGetOrderDetailsQuery(orderId)
+	console.log({ order, isLoading, error })
 	return <div>OrderScreen</div>
 }
 
