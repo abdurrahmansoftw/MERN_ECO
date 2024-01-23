0f579048a0d30883ad03b82113c6810c562e837c
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2024-01-23 15:26:47 +0600

    Dashboard

--- frontend/src/admin/Dashboard.jsx
+++ frontend/src/admin/Dashboard.jsx
@@ -1,33 +1,12 @@
 import Box from '@mui/material/Box'
-import CssBaseline from '@mui/material/CssBaseline'
 import Toolbar from '@mui/material/Toolbar'
 import Typography from '@mui/material/Typography'
 
 const Dashboard = () => {
 	return (
-		<Box sx={{ display: 'flex' }}>
-			<CssBaseline />
-			{/* Header and Sidebar are components */}
-
-			{/* Box is a component */}
-			<Box component='main' sx={{ flexGrow: 1, p: 3 }}>
-				<Toolbar />
-
-				<Typography paragraph>
-					Consequat mauris nunc congue nisi vitae suscipit. Fringilla est
-					ullamcorper eget nulla facilisi etiam dignissim diam. Pulvinar
-					elementum integer enim neque volutpat ac tincidunt. Ornare suspendisse
-					sed nisi lacus sed viverra tellus. Purus sit amet volutpat consequat
-					mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis
-					risus sed vulputate odio. Morbi tincidunt ornare massa eget egestas
-					purus viverra accumsan in. In hendrerit gravida rutrum quisque non
-					tellus orci ac. Pellentesque nec nam aliquam sem et tortor. Habitant
-					morbi tristique senectus et. Adipiscing elit duis tristique
-					sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis
-					eleifend. Commodo viverra maecenas accumsan lacus vel facilisis. Nulla
-					posuere sollicitudin aliquam ultrices sagittis orci a.
-				</Typography>
-			</Box>
+		<Box component='main' sx={{ flexGrow: 1, p: 3 }}>
+			<Toolbar />
+			<Typography paragraph>Dashboard</Typography>
 		</Box>
 	)
 }
