ba25c1bc846eec34f8a3008852f782c122fc57b2
Abdur Rahman <abdurrahmansoftw@gmail.com>
2024-01-31 22:49:44 +0600

    uploadRoutes

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import connectDB from './database/database.js'
 import { errorHandler, notFound } from './middleware/errorMiddleware.js'
 import orderRoutes from './routes/orderRoutes.js'
 import productRoutes from './routes/productRoutes.js'
+import uploadRoutes from './routes/uploadRoutes.js'
 import userRoutes from './routes/userRoutes.js'
 dotenv.config()
 
@@ -29,7 +30,7 @@ app.get('/', (req, res) => {
 app.use('/api/users', userRoutes)
 app.use('/api/products', productRoutes)
 app.use('/api/orders', orderRoutes)
-app.use('/uploads', express.static('uploads'))
+app.use('/uploads', uploadRoutes)
 
 // paypal route
 app.get('/api/config/paypal', (req, res) =>
