5879e87ee73e6e6d495ed747f5d8297a8eca52d7
Abdur Rahman <abdurrahmansoftw@gmail.com>
2024-01-31 22:51:06 +0600

    uploads folder

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,14 @@ import colors from 'colors'
 import cookieParser from 'cookie-parser'
 import dotenv from 'dotenv'
 import express from 'express'
+import path from 'path'
 import connectDB from './database/database.js'
 import { errorHandler, notFound } from './middleware/errorMiddleware.js'
 import orderRoutes from './routes/orderRoutes.js'
 import productRoutes from './routes/productRoutes.js'
 import uploadRoutes from './routes/uploadRoutes.js'
 import userRoutes from './routes/userRoutes.js'
+
 dotenv.config()
 
 const port = process.env.PORT || 5000
@@ -36,6 +38,11 @@ app.use('/uploads', uploadRoutes)
 app.get('/api/config/paypal', (req, res) =>
 	res.send({ clientId: process.env.PAYPAL_CLIENT_ID })
 )
+
+// uploads folder
+const __dirname = path.resolve()
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+
 // error middleware
 app.use(notFound)
 app.use(errorHandler)
