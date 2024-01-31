af8574b5c281e89bec244fd4f0b60adcaa728e8d
Abdur Rahman <abdurrahmansoftw@gmail.com>
2024-01-31 11:57:12 +0600

    createProduct API

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -21,4 +21,24 @@ const getProductById = asyncHandler(async (req, res) => {
 	throw new Error('Product not found')
 })
 
-export { getProductById, getProducts }
+// @docs    Create single product
+// @route   POST /api/products/
+// @access  private/admin
+const createProduct = asyncHandler(async (req, res) => {
+	const product = new Product({
+		name: 'Sample name',
+		price: 0,
+		user: req.user._id,
+		image: '/images/sample.jpg',
+		brand: 'Sample brand',
+		category: 'Sample category',
+		countInStock: 0,
+		numReviews: 0,
+		description: 'Sample description',
+	})
+
+	const createdProduct = await product.save()
+	res.status(201).json(createdProduct)
+})
+
+export { createProduct, getProductById, getProducts }
