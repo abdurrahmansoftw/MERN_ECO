3cc934b2987ecc249cd2fa3273da9d8e4b5fd8b3
Abdur Rahman <abdurrahmansoftw@gmail.com>
2024-01-31 12:20:03 +0600

    useEffect

--- frontend/src/admin/adminScreens/ProductEditScreen.jsx
+++ frontend/src/admin/adminScreens/ProductEditScreen.jsx
@@ -1,7 +1,10 @@
 import { Typography } from '@mui/material'
-import React, { useState } from 'react'
-import { useParams } from 'react-router-dom'
-import { useGetProductDetailsQuery } from '../../slices/productsApiSlice'
+import React, { useEffect, useState } from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
+import {
+	useGetProductDetailsQuery,
+	useUpdateProductMutation,
+} from '../../slices/productsApiSlice'
 
 const ProductEditScreen = () => {
 	const { id: productId } = useParams()
@@ -20,6 +23,23 @@ const ProductEditScreen = () => {
 		error,
 		refetch,
 	} = useGetProductDetailsQuery(productId)
+
+	const [updateProduct, { isLoading: updateLoading }] =
+		useUpdateProductMutation()
+
+	const navigate = useNavigate()
+
+	useEffect(() => {
+		if (product) {
+			setName(product.name)
+			setPrice(product.price)
+			setImage(product.image)
+			setBrand(product.brand)
+			setCategory(product.category)
+			setCountInStock(product.countInStock)
+			setDescription(product.description)
+		}
+	}, [product])
 
 	console.log(product)
 	return (
