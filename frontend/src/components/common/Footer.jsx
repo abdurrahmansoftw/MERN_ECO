769b8378f2ea8d51e5fd9d12bb62931189dda210
arswe <abdurrahmansoftw@gmail.com>
2024-02-23 10:39:19 +0600

    update products

--- frontend/src/components/common/Footer.jsx
+++ frontend/src/components/common/Footer.jsx
@@ -7,24 +7,14 @@ import Stack from '@mui/material/Stack'
 import TextField from '@mui/material/TextField'
 import Typography from '@mui/material/Typography'
 
-
 import FacebookIcon from '@mui/icons-material/GitHub'
 import LinkedInIcon from '@mui/icons-material/LinkedIn'
 import TwitterIcon from '@mui/icons-material/X'
+import { Copyright } from './Copyright'
 
 const logoStyle = {
   width: '140px',
   height: 'auto',
-}
-
-function Copyright() {
-  return (
-    <Typography variant='body2' color='text.secondary' mt={1}>
-      {'Copyright © '}
-      <Link href='https://mui.com/'>Sitemark&nbsp;</Link>
-      {new Date().getFullYear()}
-    </Typography>
-  )
 }
 
 export default function Footer() {
--- frontend/src/layouts/Home/ProductGrid.jsx
+++ frontend/src/layouts/Home/ProductGrid.jsx
@@ -1,5 +1,87 @@
+import {
+  Box,
+  Card,
+  CardMedia,
+  Container,
+  Grid,
+  Stack,
+  Typography,
+} from '@mui/material'
+import products from '../../data/products'
+
 const ProductGrid = () => {
-  return <div>ProductGrid</div>
+  return (
+    <Box
+      id='highlights'
+      sx={{
+        pt: { xs: 4, sm: 12 },
+        pb: { xs: 8, sm: 16 },
+        color: 'white',
+        bgcolor: '#06090a',
+      }}
+    >
+      <Container
+        sx={{
+          position: 'relative',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          gap: { xs: 3, sm: 6 },
+        }}
+      >
+        <Box
+          sx={{
+            width: { sm: '100%', md: '60%' },
+            textAlign: { sm: 'left', md: 'center' },
+          }}
+        >
+          <Typography component='h2' variant='h4'>
+            Highlights
+          </Typography>
+          <Typography variant='body1' sx={{ color: 'grey.400' }}>
+            Explore why our product stands out: adaptability, durability,
+            user-friendly design, and innovation. Enjoy reliable customer
+            support and precision in every detail.
+          </Typography>
+        </Box>
+        <Grid container spacing={2.5}>
+          {products.map((item, index) => (
+            <Grid item xs={12} sm={6} md={4} key={index}>
+              <Stack
+                direction='column'
+                color='inherit'
+                component={Card}
+                spacing={1}
+                useFlexGap
+                sx={{
+                  p: 3,
+                  height: '100%',
+                  border: '1px solid',
+                  borderColor: 'grey.800',
+                  background: 'transparent',
+                  backgroundColor: 'grey.900',
+                }}
+              >
+                <CardMedia
+                  sx={{ height: 140 }}
+                  image={item.image}
+                  title='green iguana'
+                />
+                <div>
+                  <Typography fontWeight='medium' gutterBottom>
+                    {item.name}
+                  </Typography>
+                  <Typography variant='body2' sx={{ color: 'grey.400' }}>
+                    {item.description}
+                  </Typography>
+                </div>
+              </Stack>
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
+    </Box>
+  )
 }
 
 export default ProductGrid
